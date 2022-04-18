dd7f391cb61fd2ae4045a5f49c960c17e261162f
nassim <naseem.bensalah@gmail.com>
2022-04-18 13:33:46 +0100

    implemented update in demandeCredit

---
+++ src/main/java/com/pfe/ecredit/service/DemandeHistoriqueServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pfe.ecredit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pfe.ecredit.domain.DemandeHistorique;
+import com.pfe.ecredit.repositories.DemandeHistoriqueRepository;
+
+@Service
+public class DemandeHistoriqueServiceImpl implements DemandeHistoriqueService {
+	
+	@Autowired
+	private DemandeHistoriqueRepository repo;
+
+	@Override
+	public List<DemandeHistorique> findAll() {
+		return (repo.findAll() != null)? repo.findAll() : null;
+	}
+
+	@Override
+	public List<DemandeHistorique> findByDemande(Integer id) {
+		return (repo.findAllByIdDemande(id) != null)? repo.findAllByIdDemande(id) : null;
+	}
+
+}
