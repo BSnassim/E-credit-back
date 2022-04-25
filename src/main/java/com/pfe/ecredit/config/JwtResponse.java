a3a10082c32f77e2a2f2fb8fa113758627cd6c54
nassim <naseem.bensalah@gmail.com>
2022-04-25 11:24:45 +0100

    updated login response

--- src/main/java/com/pfe/ecredit/config/JwtResponse.java
+++ src/main/java/com/pfe/ecredit/config/JwtResponse.java
@@ -14,13 +14,6 @@ public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
 	private final String token;
-	private final Integer id;
-	private final String nom;
-	private final String prenom;
-	private final String email;
-	private final Integer tel;
-	private final Date dateNais;
 	private final List<Habilitation> roles;
-	private final SiAgence agence;
 
 	}
