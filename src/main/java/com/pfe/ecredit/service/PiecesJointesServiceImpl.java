b199c5eb05952a911febf34d94a2258400232f9c
nassim <naseem.bensalah@gmail.com>
2022-04-27 09:57:13 +0100

    added file content to attachements endpoint

--- src/main/java/com/pfe/ecredit/service/PiecesJointesServiceImpl.java
+++ src/main/java/com/pfe/ecredit/service/PiecesJointesServiceImpl.java
@@ -1,9 +1,13 @@
 package com.pfe.ecredit.service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pfe.ecredit.domain.DemandePieceJointe;
 import com.pfe.ecredit.repositories.DemandePieceJointeRepository;
@@ -16,12 +20,12 @@ public class PiecesJointesServiceImpl implements PiecesJointesService {
 
 	@Override
 	public List<DemandePieceJointe> findAllPiecesJointes() {
-		return (repo.findAll() != null)? repo.findAll() : null;
+		return (repo.findAll() != null) ? repo.findAll() : null;
 	}
 
 	@Override
 	public DemandePieceJointe findPiecesJointes(Integer id) {
-		return (repo.findById(id).isPresent())? repo.findById(id).get() : null;
+		return (repo.findById(id).isPresent()) ? repo.findById(id).get() : null;
 	}
 
 	@Override
@@ -32,17 +36,30 @@ public class PiecesJointesServiceImpl implements PiecesJointesService {
 	@Override
 	public void UpdatePiecesJointes(DemandePieceJointe PJ) {
 		repo.save(PJ);
-		
+
 	}
 
 	@Override
 	public void DeletePiecesJointes(Integer id) {
 		repo.deleteById(id);
-		
+
 	}
 
 	@Override
-	public List<DemandePieceJointe> findByDemande(Integer id) {
-		return (repo.findAllByIdDemande(id) != null)? repo.findAllByIdDemande(id) : null;
+	@Transactional
+	public List<DemandePieceJointe> findByDemande(Integer id) throws FileNotFoundException, IOException {
+		List<DemandePieceJointe> list = (repo.findAllByIdDemande(id) != null) ? repo.findAllByIdDemande(id) : null;
+		try {
+			for (DemandePieceJointe i : list) {
+				FileInputStream fis = new FileInputStream("C:\\uploadedFiles\\" + id + "\\" + i.getFileName());
+				i.setFileContent(fis.readAllBytes());
+				fis.close();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 }
